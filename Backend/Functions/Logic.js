82b0f911e449eb61af5111ef0e1f974b01fdc981
moheebakhter <131138672+moheebakhter@users.noreply.github.com>
2025-04-24 13:16:05 +0500

    home commit

--- Backend/Functions/Logic.js
+++ Backend/Functions/Logic.js
@@ -1,9 +1,8 @@
 let user = require("../Collections/User")
+let bcrypt = require("bcrypt")
+
 let main_functions = { 
-
-
- 
-    Home : async function(req,res){
+     Home : async function(req,res){
         res.send("Home Page")
         res.end();
     
@@ -11,9 +10,20 @@ let main_functions = {
 
     register_user: async function (req, res) {
         try{ 
-            let user_data = new user(req.body)
+
+             let {name,email,password,age} = req.body;
+             let checkemail = await user.findOne({email : email})
+             if(checkemail){
+                return res.status(409).json({msg:"Email Already Exist"})
+
+             }
+           else{ 
+            let encrypted_pswd = bcryt.hashSync(password, 15)
+            let user_data = new user(req.body);
             let create = await user_data.save();
             res.status(200).json({ msg: "User created successfully", data: create });
+           }
+      
         
         }catch (error) {
            return res.status(501).json({ msg: error.message })
